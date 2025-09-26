f86e7f8e02c7247c9cad7519ffdb34b0587c33d4
esmail <esmailmohamedhussein0@gmail.com>
2025-09-26 17:05:44 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(message());
+    }
+    public static String message() {
+        return "Hello, GitHub Actions!";
+    }
+}
